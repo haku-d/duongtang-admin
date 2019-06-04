3ed33798a911aaed5600ff1c5ad3c656068ec9c6
Thanh Dang <thanh@clgt.vn>
2019-06-04 07:55:37 +0700

    Refactor helper module

--- src/helper.js
+++ src/helper.js
@@ -6,6 +6,24 @@ const helper = {
     }
 
     return 'Unknown Error !1!!'
+  },
+  checkNumberZero: prNum => {
+    if (prNum === 0) {
+      return 'badge badge-secondary'
+    }
+    return ''
+  },
+  checkBadge: prBadge => {
+    if (prBadge === 'Died') {
+      return 'badge badge-secondary'
+    }
+    return ''
+  },
+  checkNumberMax: prNum => {
+    if (Number(prNum) > 999) {
+      return prNum.toLocaleString('it-IT')
+    }
+    return prNum
   }
 }
 
